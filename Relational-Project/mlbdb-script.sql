0bcf073a21143c865ee5a797047ba6eff25205ef
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2024-04-30 09:03:35 -0400

    More tables

--- Relational-Project/mlbdb-script.sql
+++ Relational-Project/mlbdb-script.sql
@@ -12,10 +12,11 @@ DROP TABLE IF EXISTS FreeAgents;
 DROP TABLE IF EXISTS Teams;
 DROP TABLE IF EXISTS Stadiums;
 DROP TABLE IF EXISTS Broadcasters;
-DROP TABLE IF EXISTS Games;
+DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS BroadcastNetworks;
 DROP TABLE IF EXISTS Awards;
 DROP TABLE IF EXISTS HallOfFame;
+DROP TABLE IF EXISTS PlayerStats;
 
 -- People --
 CREATE TABLE People (
@@ -59,7 +60,7 @@ CREATE TABLE Umpires (
 -- Broadcasters --
 CREATE TABLE Broadcasters (
     PeopleID        int not null references People(PeopleID),
-    NetworkID       int not null references BroadcastNetwork(NetworkID),
+    NetworkID       int not null references BroadcastNetworks(NetworkID),
   primary key(PeopleID)
   foreign key(NetworkID)
 );
@@ -74,6 +75,7 @@ CREATE TABLE FreeAgents (
 
 -- Team --
 CREATE TABLE Team (
+    TeamID                 int not null
     PeopleID               int not null references People(PeopleID),
     StadiumID              int not null references Stadiums(StadiumID),
     primaryColor           text not null,
@@ -83,7 +85,7 @@ CREATE TABLE Team (
     doubleA_Affiliate      text not null,
     singleA_Affiliate      text not null,
     highA_Affiliate        text not null,
-  primary key(PeopleID)
+  primary key(TeamID, PeopleID)
   foreign key(StadiumID)
 );
 
@@ -94,4 +96,62 @@ CREATE TABLE Stadiums (
     seatingCapacity     int not null,
     stadiumName         text not null,
   primary key(StadiumID)
+);
+
+-- Game --
+CREATE TABLE Game (
+    GameDate            date not null,
+    PeopleID            int not null references People(PeopleID),
+    weather             weather not null,
+    NetworkID           int not null references BroadcastNetworks(NetworkID),
+    Team1ID             int not null references Team(TeamID),
+    Team2ID             int not null references Team(TeamID),
+  primary key(GameDate, PeopleID)
+  foreign key(NetworkID, Team1ID, Team2ID)
+);
+
+-- Broadcast Networks --
+CREATE TABLE BroadcastNetworks(
+    NetworkID       int not null,
+    channelNumber   int not null,
+    networkName     text not null,
+  primary key(NetworkID)
+);
+
+-- Awards --
+CREATE TABLE Awards(
+    AwardID         int not null,
+    Award_Name      text not null,
+    Award_Winner    text not null,
+  primary key(AwardID)
+);
+
+-- Hall of Fame --
+CREATE TABLE HallOfFame(
+    HallID          int not null,
+    dateInducted    date not null,
+    PeopleID        int not null references People(PeopleID),
+  primary key(HallID, PeopleID)
+);
+
+-- Player Stats --
+CREATE TABLE PlayerStats (
+    PeopleID        int not null references People(PeopleID),
+    atBats          int,
+    hits            int,
+    strikeouts      int,
+    battingAverage  int, -- Calculate 
+    singles         int,
+    doubles         int,
+    triples         int,
+    homeruns        int,
+    walks           int,
+    onBasePercentage int, -- Calculate 
+    pitchingStrikeouts   int,
+    inningsPitched       int,
+    strikeoutsPerNine    int, -- Calculate
+    earnedRunAverage     int, -- Calculate
+    pitchingWins         int,
+    pitchingLosses       int,
+  primary key(PeopleID)
 );
