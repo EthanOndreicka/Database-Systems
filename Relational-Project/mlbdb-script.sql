79b7358e876ed37d6e7d1ee2ee5d6464818ab6dd
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2024-04-30 20:12:04 -0400

    Done with inserts I hope

--- Relational-Project/mlbdb-script.sql
+++ Relational-Project/mlbdb-script.sql
@@ -27,6 +27,8 @@ CREATE TYPE division AS ENUM ('NL East', 'NL Central', 'NL West', 'AL East', 'AL
 
 CREATE TYPE orientation AS ENUM ('lefty', 'righty', 'ambidextrous');
 
+CREATE TYPE orientation AS ENUM ('rainy', 'cloudy', 'sunny', 'snowy', 'windy');
+
 -- Table Creation Statements
 
 -- People --
@@ -109,12 +111,11 @@ CREATE TABLE Stadiums (
 -- Game --
 CREATE TABLE Game (
     GameDate            date not null,
-    PeopleID            int not null references People(PeopleID),
     weather             weather not null,
     NetworkID           int not null references BroadcastNetworks(NetworkID),
     Team1ID             int not null references Team(TeamID),
     Team2ID             int not null references Team(TeamID),
-  primary key(GameDate, PeopleID)
+  primary key(GameDate)
   foreign key(NetworkID, Team1ID, Team2ID)
 );
 
@@ -192,7 +193,7 @@ CREATE TABLE BroadcastsFor (
 );
 
 -- Award Won --
-CREATE TABLE awardWon (
+CREATE TABLE AwardsWon (
     PeopleID int not null references People(PeopleID),
     AwardsID int not null references Awards(AwardsID);
 )
@@ -207,10 +208,10 @@ VALUES             (1, 'John', 'Lobster', '1995-07-22', '2018-04-12', NULL),
                    (5, 'Alan', 'Labouseur', '1985-02-14', '2006-03-30', 3),
                    (6, 'Paul', 'Boats', '1981-03-17', '2002-06-22', NULL),
                    (7, 'Michael', 'Coral', '1989-10-10', '2002-04-12', 4),
-                   (8, 'Josh', 'Fish', '2003-02-17', '2024-3-29', NULL),
+                   (8, 'Josh', 'Fish', '2003-02-17', '2024-03-29', NULL),
                    (9, 'Randy', 'Flounder', '1995-07-26', '2017-10-01', 5),
                    (10, 'Carlos', 'Hook', '1988-01-19', '2012-06-05', NULL),
-                   (11, 'Tim', 'Sea', '2001-03-13', '2023-7-12', NULL),
+                   (11, 'Tim', 'Sea', '2001-03-13', '2023-07-12', NULL),
                    (12, 'Frank', 'Kelp', '1973-11-24', '1998-05-23', NULL),
                    (13, 'Kyle', 'Dolphin', '1983-12-02', '2006-08-17', 6),
                    (14, 'Dominick', 'Osyter', '1968-05-01', '1993-02-12', 7),
@@ -225,16 +226,35 @@ VALUES             (1, 190.500, 80.000, 'Second Base', 'lefty', 'righty'),
                    (9, 120.700, 72.991, 'Right Field', 'righty', 'lefty'),
                    (11, 166.388, 59.140, 'Catcher', 'righty', 'righty');
 
+INSERT INTO PlaysFor (PeopleID, TeamID, start_date, end_date)
+VALUES             (1, 1, '2019-05-22', NULL),
+                   (2, 3, '2016-08-12', NULL),
+                   (4, 3, '2016-08-30', NULL),
+                   (5, 4, '2020-06-17', NULL),
+                   (8, 4, '2024-03-29', NULL),
+                   (9, 2, '2017-10-01', '2023-09-15'),
+                   (11, 1, '2023-07-12', NULL);
+
 
 INSERT INTO Coaches (PeopleID, gamesCoached)
 VALUES             (6, 1378),
                    (10, 322),
                    (14, 2001);
 
+INSERT INTO CoachesFor (PeopleID, TeamID, start_date, end_date)
+VALUES             (6, 2, '2008-07-19', '2022-09-18'),
+                   (10, 1, '2014-04-01', NULL),
+                   (14, 4, '2000-05-22', NULL);
+
 INSERT INTO Broadcasters (PeopleID, gamesBroadcasted)
 VALUES             (3, 183),
                    (12, 2001),
                    (13, 971),
+
+INSERT INTO BroadcastsFor (PeopleID, NetworkID, start_date, end_date)
+VALUES             (3, 3, '2023-01-19', NULL),
+                   (12, 1, '2000-10-02', NULL),
+                   (13, 2, '2015-03-27', '2021-09-12');
 
 INSERT INTO Umpires (PeopleID, gamesUmpired)
 VALUES             (7, 287),
@@ -273,4 +293,58 @@ VALUES             (1, 'Silver Slugger'),
                    (4, 'Coach of the Year'),
                    (5, 'Broadcaster of the Year');
 
-INSERT INTO 
+INSERT INTO AwardsWon (PeopleID, AwardsID)
+VALUES             (5 , 3),
+                   (12, 5),
+                   (14, 4),
+                   (8, 2),
+                   (4, 1);
+
+INSERT INTO Game (GameDate, weather, NetworkID, Team1ID, Team2ID)
+VALUES             ('2024-05-12', sunny, 3, 1, 4),
+                   ('2024-05-13', rainy, 2, 2, 4),
+                   ('2024-05-14', snowy, 2, 1, 4),
+                   ('2024-05-15', sunny, 1, 3, 4),
+                   ('2024-05-17', cloudy, 3, 4, 1),
+                   ('2024-05-18', sunny, 1, 4, 3);
+
+INSERT INTO PlayerStats (PeopleID, atBats, hits, strikeouts, singles, doubles, triples, homeruns, walks, pitchingStrikeouts, inningsPitched, earnedRuns, pitchingWins, pitchingLosses)
+VALUES             (1, 891, 212, 189, 184, 20, 3, 5, 102, NULL, NULL, NULL, NULL, NULL),
+                   (2, 2082, 550, 289, 300, 74, 42, 134, 200, NULL, NULL, NULL, NULL, NULL),
+                   (4, 999, 333, 44, 222, 56, 18, 37, 66, NULL, NULL, NULL, NULL, NULL),
+                   (5, 1000, 1000, 0, 0, 0, 0, 1000, 0, 2700, 900, 0, 100, 0),
+                   (8, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 72, 62, 13, 7, 2)
+                   (9, 3038, 891, 388, 588, 120, 51, 132, 204, NULL, NULL, NULL, NULL, NULL),
+                   (11, 512, 220, 61, 78, 40, 9, 93, 36, NULL, NULL, NULL, NULL, NULL);
+
+
+-- Player Stats --
+CREATE TABLE PlayerStats (
+    PeopleID        int not null references People(PeopleID),
+    atBats          int,
+    hits            int,
+    strikeouts      int,
+    battingAverage  decimal(5,3) as (CASE WHEN atBats > 0 THEN CAST (hits as decimal(5,0)) / atBats ELSE NULL END), -- Calculate 
+    singles         int,
+    doubles         int,
+    triples         int,
+    homeruns        int,
+    walks           int,
+    onBasePercentage decimal (5,3) as (CASE WHEN atBats > 0 THEN (CAST(hits AS decimal(5,0))+ walks) / atBats ELSE NULL END), 
+    pitchingStrikeouts   int,
+    inningsPitched       int,
+    strikeoutsPerNine    decimal(5,2) as (CASE WHEN inningsPitched > 0 THEN (CAST(pitchingStrikeouts AS decimal(5,0)) / inningsPitched) * 9),
+    earnedRuns           int,
+    earnedRunAverage     decimal(5,2) as (CASE WHEN inningsPitched > 0 THEN (CAST(earnedRuns AS decimal(5,0)) / inningsPitched) * 9 ELSE NULL END),
+    pitchingWins         int,
+    pitchingLosses       int,
+  primary key(PeopleID)
+);
+
+(1, 190.500, 80.000, 'Second Base', 'lefty', 'righty'),
+                   (2, 165.100, 65.317, 'Center Field', 'righty', 'righty'),
+                   (4, 183.439, 95.467, 'Shortstop', 'righty', 'ambidextrous'),
+                   (5, 808.808, 808.808, 'Pitcher', 'righty', 'righty'),
+                   (8, 199.691, 101.803, 'Pitcher', 'lefty', 'lefty'),
+                   (9, 120.700, 72.991, 'Right Field', 'righty', 'lefty'),
+                   (11, 166.388, 59.140, 'Catcher', 'righty', 'righty');
